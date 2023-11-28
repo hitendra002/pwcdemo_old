77ad4f4040e2aa8e8c55fa62bb26377ccf410b1d
Hitendra <hitendrapatel002@gmail.com>
2023-11-28 12:44:11 +0100

    added rest apis

---
+++ API_Test/test_orange_hrm_api.py
@@ -0,0 +1,58 @@
+import requests
+import json
+from bs4 import BeautifulSoup
+
+def login():
+
+    try:
+        # Session initializing
+        session = requests.Session()
+        res1 = session.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+        print("First login",res1)
+        assert res1.status_code == 200, "Initial Login failed"
+
+        # Fetch CSRF token from html response
+        token = BeautifulSoup(res1.text, 'html.parser').select_one('auth-login')[':token']
+        print(f"CSRF token: {token}")
+
+        # Authenticating using CSRF and Credential
+        payload = {
+            "_token": token,
+            "username": "Admin",
+            "password": "admin123"
+        }
+        auth_res = session.post("https://opensource-demo.orangehrmlive.com/web/index.php/auth/validate", data=payload)
+        assert auth_res.status_code == 200, "Authentication failed"
+
+        # Add Emplyoee into HRM
+        emplyoeeData = {
+                "firstName": "John",
+                "middleName": "wick",
+                "lastName": "Smith",
+                "empPicture": None,
+                "employeeId": "88256"
+            }
+        add_res = session.post("https://opensource-demo.orangehrmlive.com/web/index.php/api/v2/pim/employees", data=emplyoeeData)
+        print("Add emp",add_res)
+        assert add_res.status_code == 200, "Add employee failed"
+        data = add_res.json()
+        print("Add Employee response", data)
+        empID = data["data"]["empNumber"]
+
+        # Search employee with ID
+        search_emp_res = session.get(f"https://opensource-demo.orangehrmlive.com/web/index.php/api/v2/pim/employees/{empID}/personal-details")
+        assert search_emp_res.status_code == 200, "Search employee failed"
+        data = search_emp_res.json()
+        print("Search Employee response", data)
+        # Assert Employee details
+        assert data["data"]["employeeId"] == emplyoeeData["employeeId"], "Employee id mismatch"
+        assert data["data"]["lastName"] == emplyoeeData["lastName"], "Employee lastname mismatch"
+        assert data["data"]["firstName"] == emplyoeeData["firstName"], "Employee firstname mismatch"
+        assert data["data"]["middleName"] == emplyoeeData["middleName"], "Employee middlename mismatch"
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+
+
+if __name__ == "__main__":
+    login()
